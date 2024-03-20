5bdc7b5d71cddfa89086f4945fe14d2b0aabbb80
jose2004orge <163520607+jose2004orge@users.noreply.github.com>
2024-03-20 17:27:27 +0100

    Update Pedidos.c

--- ESIZON/Pedidos.c
+++ ESIZON/Pedidos.c
@@ -1,1 +1,20 @@
+#include<stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <locale.h>
+#include <windows.h>
+#include "Perfil.h"
 
+/*typedef struct{
+    char id_pedido[8];
+    char fecha_pedido[11];
+    char id_cliente[8];
+    char lugar[9];
+    char id_locker[11];
+    char id_cod[11];
+} pedidos;
+*/
+
+void pedidoscliente(){
+    
+}
