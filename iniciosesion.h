e4c1bea1826937b88629fef5e77798f4630db1b9
Jose Luis Ramirez <127350032+elxelu@users.noreply.github.com>
2024-04-10 17:45:47 +0200

    Add files via upload

--- iniciosesion.h
+++ iniciosesion.h
@@ -4,3 +4,6 @@
 #include "Registro.h"
 
 int iniciosesion(cliente* cliente, int* id ,admin_prov* adminprov, transportista* transportista ) // esto es un prototipo
+void registarPerfilCliente(cliente * cliente, int id) // hay que poner el provtraps o como se llame
+void registrarPerfilProv(admin_prov * admin, int id, int numprov) // hay que poner el provtraps o como se llame
+void registrarPerfilTransport(transportista* transportista, int id) // hay que poner el numtraps o como se llame
