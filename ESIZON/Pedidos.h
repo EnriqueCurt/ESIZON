39e38a4a7620fd867794dc7a3c5a27c4f3e9fa34
jose2004orge <163520607+jose2004orge@users.noreply.github.com>
2024-04-18 19:47:09 +0200

    Update Pedidos.h

--- ESIZON/Pedidos.h
+++ ESIZON/Pedidos.h
@@ -2,8 +2,13 @@
 #define PEDIDOS_H
 #include "Structs.h"
 
-void ClientePedidos(pedidos *pedidos, int numpedidos);
+void ClientePedidos(pedidos *pedidos, int *numpedidos);
 void AdminPedidos(pedidos *pedidos, int *numpedidos);
 void ProveedorPedidos(pedidos *pedidos, int *numpedidos, admin_prov *admin, int id);
-
+void ModificarPedido(pedidos *pedido,productos_pedidos *productopedido,productos *producto,int *numpedidos,int *numproductos,int id);
+void ConsultarPedido(pedidos *pedido,productos_pedidos *productopedido,productos *producto,int *numpedidos,int *numproductos,int id);
+void EliminarPedido(pedidos *pedido,productos_pedidos *productopedido,productos *producto,int *numpedidos,int *numproductos,int id);
+void RealizarPedido(pedidos *pedido,productos_pedidos *productopedido,productos *producto,int *numpedidos,int *numproductos,int id);
+void AsignarLocker();
+void AsignarTransportista();
 #endif
