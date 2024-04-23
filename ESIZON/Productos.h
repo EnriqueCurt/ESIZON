d95b657d5be2cb97513998edf97a8059415941ef
jose2004orge <163520607+jose2004orge@users.noreply.github.com>
2024-04-23 17:43:10 +0200

    Update Productos.h

--- ESIZON/Productos.h
+++ ESIZON/Productos.h
@@ -1,16 +1,12 @@
+#ifndef PRODUCTOS_H
+#define PRODUCTOS_H
 #ifndef PEDIDOS_H
 #define PEDIDOS_H
 #include "Structs.h"
 
-void ClientePedidos(pedidos *pedidos, int *numpedidos);
-void AdminPedidos(pedidos *pedidos, int *numpedidos);
-void ProveedorPedidos(pedidos *pedidos, int *numpedidos, admin_prov *admin, int id);
-void ModificarPedidoAdmin(pedidos *pedido,productos_pedidos *productopedido,productos *producto,int *numpedidos,int *numproductos,int id;
-void ModificarPedidoPropioTransportista(transportistas *transportista,int *numtransportista,pedidos *pedido,productos_pedidos *productopedido,productos *producto,int *numpedidos,int *numproductos,int id);
-void ModificarPedidoPropioCliente(clientes *cliente,int *numclientes,pedidos *pedido,productos_pedidos *productopedido,productos *producto,int *numpedidos,int *numproductos,int id);
-void ConsultarPedido(pedidos *pedido,productos_pedidos *productopedido,productos *producto,int *numpedidos,int *numproductos,int id);
-void EliminarPedido(pedidos *pedido,productos_pedidos *productopedido,productos *producto,int *numpedidos,int *numproductos,int id);
-void RealizarPedido(pedidos *pedido,productos_pedidos *productopedido,productos *producto,int *numpedidos,int *numproductos,int id);
-void AsignarLocker();
-void AsignarTransportista();
+void Cliente_MostrarProductos(productos *productos, int numProductos);
+void Admin_GestionarProductos(productos *productos, int *numProductos);
+void Proveedor_GestionarProductos(productos *productos, int *numProductos, admin_prov *admin, int id);
+
+
 #endif
